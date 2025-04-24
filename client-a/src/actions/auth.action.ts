621fe1381fbf33550c9b7b843223e91318246337
owaisatcarbonteq <owais.ahsan@carbonteq.com>
2025-04-24 18:26:11 +0500

    add .env variable + separate .env files

--- client-a/src/actions/auth.action.ts
+++ client-a/src/actions/auth.action.ts
@@ -5,7 +5,7 @@ import { isRedirectError } from "next/dist/client/components/redirect-error"
 
 export const logOutAction = async () => {
   try {
-    await signOut({ returnTo: "http://localhost:3000/" })
+    await signOut({ returnTo: process.env.APP_URI })
   } catch (error) {
     if (isRedirectError(error)) throw error
     console.error("Auth Actions", "Logout", error)
--- client-b/src/actions/auth.action.ts
+++ client-b/src/actions/auth.action.ts
@@ -5,7 +5,7 @@ import { isRedirectError } from "next/dist/client/components/redirect-error"
 
 export const logOutAction = async () => {
   try {
-    await signOut({ returnTo: "http://localhost:3001/" })
+    await signOut({ returnTo: process.env.APP_URI })
   } catch (error) {
     if (isRedirectError(error)) throw error
     console.error("Auth Actions", "Logout", error)
